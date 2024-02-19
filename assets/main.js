bf3d048c4b6eb2d1a453f76b5d8380115d42ffa5
nicodellolio <nico.dellolio18@gmail.com>
2024-02-19 16:52:10 +0100

    add prompt

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+// chiedere al passeggero (utente) il numero di chilometri da fare
+let kmToDo = Number(prompt("Inserisci i km che desideri percorrere"));
+console.log(kmToDo);
+
+// chiedere al passeggero (utente) la propria età
+const userAge = Number(prompt("Inserisci la tua età"));
+console.log(userAge);
